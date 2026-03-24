8c8910628530c27ac1d418902214e13cdcc9581a
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-03-24 10:04:23 +0530

    Update qr_generator.py

--- code/qr_generator.py
+++ code/qr_generator.py
@@ -1,2 +1,14 @@
 import qrcode  # import the QR code library to generate code.
 from datetime import datetime  # import datetime to create unique file names using timestamp
+
+def generate_qr(data, fill_color='black', back_color='white'):  # this function generates a QR code image from given data.
+    # parameters : data(str): the text or url to encode inside the QR.
+    # fill_color (str) : color of the QR pattern.
+    # back_color (str) : background color of QR.
+    # return : 
+    # filename(str) : name of the saved QR image file
+    
+    
+    if not data.strip():             # check if the input is empty or just spaces                                 
+        raise ValueError("input cannot be empty.")   # raise error if the user enters empty input.
+
