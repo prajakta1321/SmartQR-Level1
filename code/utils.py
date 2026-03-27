608dfad40489c56a3cda895d49ab83e9a81b4149
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-03-27 10:37:41 +0530

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -6,4 +6,5 @@ def is_valid_url(url):   # this function checks whether the given string looks l
         r'([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,6})'   # domain extension like .cpm, .org
         r'(\/\S*)?$'        # optional path after domain
     )
+    return re.match(pattern, url)    # checks if the input matches the patter; returns True if url matches pattern, other wise false
 
