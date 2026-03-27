dc7842ee69dc683f07bf8c9f355c9aadd25d98a1
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-03-27 10:35:46 +0530

    Create utils.py

---
+++ code/utils.py
@@ -0,0 +1,9 @@
+import re  # import re (regular expression) to validate url format
+
+def is_valid_url(url):   # this function checks whether the given string looks like a valid url or no
+    pattern = re.compile(   # define a regular pattern for basic url validation
+        r'^(https?:\/\/)?'  # optional http:// or https://
+        r'([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,6})'   # domain extension like .cpm, .org
+        r'(\/\S*)?$'        # optional path after domain
+    )
+
